298ec8614a4bf3201c3cf5ec6a0765ca4fdef37d
brian <riceb53@gmail.com>
2020-11-02 19:43:12 -0500

    add strftime and calculated output

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -2,6 +2,8 @@ class Api::ExamplePagesController < ApplicationController
   def hello_action
     # web response
     p "AHHHHHHHHHHH"
+    @output = 12 * 200
+    @time = Time.now.strftime("%a, %e %b %Y %H:%M:%S %z")
     render 'hello.json.jb'
   end
 end
